6f63da8593d237e9158a4703b829d8b95493651a
prrrri <priscilaigarashi@icloud.com>
2023-10-14 14:34:03 -0300

    continuando o filtro

--- src/main/java/com/igarashipuri/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/igarashipuri/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,42 @@
 package com.igarashipuri.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter{
+public class FilterTaskAuth extends OncePerRequestFilter{
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);
-      }
+        //Pegar a autenticação (usuário e senha)
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
+
+        //editando a forma como é apresentado o usuário e senha
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+
+        filterChain.doFilter(request, response);
+  }
   
 }
